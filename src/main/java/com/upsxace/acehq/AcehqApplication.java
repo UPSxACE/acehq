3602da9545723a050d32602dbe1cfd7d261319fc
Eduardo Botelho <eduardo11224b@gmail.com>
2025-08-31 21:59:19 +0100

    initial setup

---
+++ src/main/java/com/upsxace/acehq/AcehqApplication.java
@@ -0,0 +1,13 @@
+package com.upsxace.acehq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcehqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcehqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upsxace/acehq/AcehqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upsxace.acehq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcehqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
